c913d2d98679c20bdfb94a2e10e71389fe32f456
John Li <lij19@wit.edu>
2019-04-06 12:10:50 -0400

    altered things in StorySections to better test the scene switching

--- src/SceneSwitch.java
+++ src/SceneSwitch.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -9,8 +8,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
-
 
 
 public class SceneSwitch implements Initializable{
@@ -21,6 +18,8 @@ public class SceneSwitch implements Initializable{
 	Button testBtn;
 	@FXML 
 	Button quitBtn;
+	@FXML
+	Button rpgBtn;
 	
 	private Stage stage;
 	
@@ -36,7 +35,7 @@ public class SceneSwitch implements Initializable{
 		 root = (Pane) FXMLLoader.load(getClass().getResource("minefield.fxml"));
 		 Scene scene = new Scene(root);
 		 stage.setScene(scene);
-		 System.out.println("minefeild.fxml opened");
+		 System.out.println("minefield.fxml opened");
 	 }
 	 
 	 @FXML	
@@ -54,6 +53,17 @@ public class SceneSwitch implements Initializable{
 		 Scene scene = new Scene(root);
 		 stage.setScene(scene);
 		 System.out.println("mainMenud.fxml opened");
+	 }
+	 
+	 @FXML
+	 private void openRpgStory() throws IOException{
+		 stage = (Stage) rpgBtn.getScene().getWindow();
+		 Pane root = (Pane) FXMLLoader.load(getClass().getResource("/Story/StorySections.fxml"));
+		 Scene scene = new Scene(root);
+		 stage.setScene(scene);
+		 System.out.println("StorySections.fxml opened");
+		 
+		 
 	 }
 
 }
--- src/Story/Sections.java
+++ src/Story/Sections.java
@@ -1,5 +1,13 @@
 package Story;
 
-public class Sections {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
+public class Sections {
+	@FXML
+	Button btn1;
+	
+	public void printing() {
+		System.out.println("hi");
+	}
 }
